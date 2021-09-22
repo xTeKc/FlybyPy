cbb48da6a5ed32cbf4b7ad3a3966df7f86f7b915
xTeKc <bcd3v@pm.me>
2021-09-22 09:28:03 -0400

    create var ack list and append to empty

--- fileint.py
+++ fileint.py
@@ -1,6 +1,8 @@
 import netfilterqueue
 import scapy.all as scapy
 
+ack_list = []
+
 def process_packet():
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
@@ -8,6 +10,7 @@ def process_packet():
             print("HTTP Request")
             if ".exe" in scapy_packet[scapy.Raw].load:
                 print("[+] exe Request")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
             print(scapy_packet.show())
         elif scapy_packet[scapy.TCP].sport == 80:
             print("HTTP Response")
